8cbc87d58612a5b02a7a4235e6fa565047525700
Owen Harding <owenrharding@gmail.com>
2024-09-16 21:47:56 +1000

    Added command line arg parsing

--- IDS.py
+++ IDS.py
@@ -1,1 +1,33 @@
 # Intrusion Detection System
+
+"""
+This program would read two files:
+1) one file includes intrusion detection rules (like rules used in Snort) and 
+2) the other file is the .pcap file that contains all the packets that your 
+   program would go through to check if any or some of them violates the rules.
+
+Both files will be passed into your Python program (IDS.py) through the 
+Command-Line Argument.
+
+The start of IDS.py would be:
+$python3 IDS.py <path_to_the_pcap_file> <path_to_the_IDS_rules>
+Both paths need to be absolute paths.
+"""
+
+import sys
+from scapy.all import *
+from datetime import datetime
+
+def main():
+    # Check if the number of arguments in command line is correct.
+    if len(sys.argv) != 3:
+        print("Incorrect number of arguments.")
+
+    # Parse and extract command line arguments.
+    execFile = sys.argv[0]
+    pcapFile = sys.argv[1]
+    rulesFile = sys.argv[2]
+
+
+if __name__ == '__main__':
+    main()
